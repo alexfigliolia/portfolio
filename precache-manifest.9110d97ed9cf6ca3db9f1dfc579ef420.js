87acbe79c0f95eaff7078f401a0700a501366fd7
alexfigliolia <alexfigliolia@gmail.com>
2019-06-23 14:41:16 -0400

    Updates

---
+++ precache-manifest.9110d97ed9cf6ca3db9f1dfc579ef420.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3480a8d63f348bca8e789e1f8db43b44",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7b0339f11da5a22297cd",
+    "url": "/portfolio/static/css/main.2c29a397.chunk.css"
+  },
+  {
+    "revision": "1324ea0ca254bd670dd6",
+    "url": "/portfolio/static/js/2.60f7bb18.chunk.js"
+  },
+  {
+    "revision": "af6dee3be14b51040c41",
+    "url": "/portfolio/static/js/3.e8203b78.chunk.js"
+  },
+  {
+    "revision": "4a1e9f171a2a7c5e6bf7",
+    "url": "/portfolio/static/js/4.11c633a7.chunk.js"
+  },
+  {
+    "revision": "aed91aa9cb59d374d41c",
+    "url": "/portfolio/static/js/5.96376139.chunk.js"
+  },
+  {
+    "revision": "7b0339f11da5a22297cd",
+    "url": "/portfolio/static/js/main.4532b1f3.chunk.js"
+  },
+  {
+    "revision": "b55a0300c4ceef1c0ad5",
+    "url": "/portfolio/static/js/runtime~main.46655a69.js"
+  },
+  {
+    "revision": "3214b642ff78d264e13edd0872840e69",
+    "url": "/portfolio/static/media/COMPR-sm.3214b642.jpg"
+  },
+  {
+    "revision": "75c30d9398bb2025b2b6ebb07303b750",
+    "url": "/portfolio/static/media/COMPR.75c30d93.jpg"
+  },
+  {
+    "revision": "fc6246858fc67310e0f9fc4b80d2ae55",
+    "url": "/portfolio/static/media/cityweb-small.fc624685.jpg"
+  },
+  {
+    "revision": "ebb0edcb063f27c8e384a4d20ea14b0c",
+    "url": "/portfolio/static/media/cityweb.ebb0edcb.jpg"
+  },
+  {
+    "revision": "b4692dee2ee1a651775c2165a0188c0f",
+    "url": "/portfolio/static/media/dog-small.b4692dee.jpg"
+  },
+  {
+    "revision": "19cf8ff218cdd8f51459bdf9e9ddd89e",
+    "url": "/portfolio/static/media/dog.19cf8ff2.jpg"
+  },
+  {
+    "revision": "e45e76ce18b415a483a2caf3a277b706",
+    "url": "/portfolio/static/media/down.e45e76ce.svg"
+  },
+  {
+    "revision": "32463de5a46bef76762a262b51af83bb",
+    "url": "/portfolio/static/media/gnocchi-small.32463de5.jpg"
+  },
+  {
+    "revision": "076e386f209bc23501d96c04c731bccd",
+    "url": "/portfolio/static/media/gnocchi.076e386f.jpg"
+  },
+  {
+    "revision": "e461c001ca7480a9d5fe7073cc050383",
+    "url": "/portfolio/static/media/left.e461c001.svg"
+  },
+  {
+    "revision": "e5c0925158bb45f7a43dd17d8f0c27d6",
+    "url": "/portfolio/static/media/nightsky-small.e5c09251.jpg"
+  },
+  {
+    "revision": "98da70e595bc329a0bfbc35ef080e355",
+    "url": "/portfolio/static/media/nightsky.98da70e5.jpg"
+  },
+  {
+    "revision": "517efdab99ceb6802093340cf408b045",
+    "url": "/portfolio/static/media/react-desktop2.517efdab.jpg"
+  },
+  {
+    "revision": "1cf802527aea906b0e50f45dc5b12775",
+    "url": "/portfolio/static/media/react-small.1cf80252.jpg"
+  },
+  {
+    "revision": "d65ffcf0b35cfb3a8c4ffdcd9faa7a01",
+    "url": "/portfolio/static/media/republic-large.d65ffcf0.jpg"
+  },
+  {
+    "revision": "bbf6c7c6297a97dd86f9533c01757510",
+    "url": "/portfolio/static/media/republic-small.bbf6c7c6.jpg"
+  },
+  {
+    "revision": "79d2b7b362c5a64716ac8ac0cb9063c3",
+    "url": "/portfolio/static/media/right.79d2b7b3.svg"
+  },
+  {
+    "revision": "bb8d483fb06e8b82177e53af2129a411",
+    "url": "/portfolio/static/media/skedge-desktop2.bb8d483f.jpg"
+  },
+  {
+    "revision": "fc7330064457091f65e77ff41d38eb38",
+    "url": "/portfolio/static/media/skedge-small.fc733006.jpg"
+  },
+  {
+    "revision": "e3583ba4ecedb87b971ce24979722331",
+    "url": "/portfolio/static/media/sv-large.e3583ba4.jpg"
+  },
+  {
+    "revision": "0e44be243e57c8d329909ad9caedd360",
+    "url": "/portfolio/static/media/sv-small.0e44be24.jpg"
+  },
+  {
+    "revision": "85bbbb3f51c600a6fff033404e7bb5bc",
+    "url": "/portfolio/static/media/tree-small.85bbbb3f.jpg"
+  },
+  {
+    "revision": "f06e307b29a675d7968fc14a69763b21",
+    "url": "/portfolio/static/media/tree.f06e307b.jpg"
+  },
+  {
+    "revision": "de89b74962e758cea409d1fa6379fc44",
+    "url": "/portfolio/static/media/up.de89b749.svg"
+  },
+  {
+    "revision": "57dc6a2b6d813b7aa54d1f9190548b71",
+    "url": "/portfolio/static/media/winter1.57dc6a2b.jpg"
+  },
+  {
+    "revision": "b5fe43c6fd3a0bdcb1be097ce3ad7c48",
+    "url": "/portfolio/static/media/winter3.b5fe43c6.jpg"
+  },
+  {
+    "revision": "bfdc87f37e3e74c617440ba527f497cd",
+    "url": "/portfolio/static/media/wordClouds-small.bfdc87f3.png"
+  },
+  {
+    "revision": "f0afd889c6fac60edac122a7f70f2f2e",
+    "url": "/portfolio/static/media/wordClouds.f0afd889.png"
+  }
+]);

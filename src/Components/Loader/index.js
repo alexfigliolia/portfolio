61a1634f6a29f905a5e7f8d32aba93ac58e48e1d
alexfigliolia <alexfigliolia@gmail.com>
2019-06-24 15:51:41 -0400

    Fix loader rendering issue

--- src/Components/Loader/index.js
+++ src/Components/Loader/index.js
@@ -9,29 +9,22 @@ class Loader extends Component {
 
   render() {
     return (
-      <div 
+      <section
         id='loader' 
         className={this.props.classes}>
-        <div className='profile-main-loader'>
-          <div className='loads'>
-            <svg 
-              className='svg'
-              version='1' 
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 28 28'>
-              <g className='qp-circular-loader'>
-                <path 
-                  className='qp-circular-loader-path' 
-                  fill='none' 
-                  d='M 14,1.5 A 12.5,12.5 0 1 1 1.5,14' 
-                  strokeWidth='1.5'
-                  stroke='#fff'
-                  strokeLinecap='round'></path>
-              </g>
-            </svg>
-          </div>
-        </div>
-      </div>
+        <ul className='spinner'>
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
+      </section>
     );
   }
 }
